e90ca6f871eb052c2d33ebdee7035e01aa4d21ac
SinaisAna <anaguanda20@gmail.com>
2021-01-18 22:01:06 -0500

    home login angular

---
+++ apiClient/src/app/app.module.ts
@@ -0,0 +1,42 @@
+ import { BrowserModule } from '@angular/platform-browser';
+ import { NgModule } from '@angular/core';
+
+ import { AppComponent } from './app.component';
+ import { UserComponent } from './user/user.component';
+ import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HomeComponent } from './home/home.component';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
+ @NgModule({
+   declarations: [
+     AppComponent,
+    UserComponent,
+     LoginComponent,
+     HomeComponent,
+   ],
+   imports: [
+     BrowserModule,
+     BrowserAnimationsModule,
+     FlexLayoutModule,
+     MatFormFieldModule,
+     MatInputModule,
+     MatButtonModule,
+     MatCardModule,
+     AppRoutingModule,
+     MatToolbarModule,
+     MatIconModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
+ })
+ export class AppModule { }
