6e0d20ffec22369e21694d5f412c0b43f8f69b57
SinaisAna <anaguanda20@gmail.com>
2021-01-20 14:56:53 -0500

    avances con cartas

--- apiClient/src/app/home/home.component.ts
+++ apiClient/src/app/home/home.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public products: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getProducts("desa");
   }
+  
+  clickfood(){
+    alert("toy funcionando vieja lesbiana");
+  }
+
+  getProducts(typeSelect): void {
+    // this.productsService.getListProduct().subscribe(
+      // response => {
+         this.products = [{name: "esauDesa",price:2000,type:"desa"},{name: "esau",price:2000,type:"nodesa"}];
+         const breakfast = this.products.filter(products => products.type === typeSelect);
+        this.products = breakfast;
+      // }
+    // );
+    }
+
 
 }
